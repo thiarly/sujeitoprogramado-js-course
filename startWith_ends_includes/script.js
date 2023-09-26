e9520efe95e70e4e4ba6671aa199146458ecc01c
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-09-26 17:19:48 -0300

    requisições http

---
+++ startWith_ends_includes/script.js
@@ -0,0 +1,17 @@
+// includes, startsWith, endsWith
+
+
+// let nomes = ['José', 'Maria', 'João'];
+
+// console.log(nomes.includes('Thiarly'));
+
+// if(nomes.includes('José')){
+//     console.log('José está na lista');
+// }else{
+//     console.log('José não está na lista');
+// }
+
+
+let nomes = "José";
+
+console.log(nomes.startsWith('Jos'));
