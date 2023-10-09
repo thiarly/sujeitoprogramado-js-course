71741dd1c3e117d9d1400c7591a7fe40a9e9adc2
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-10-09 15:26:30 -0300

    pagina de criar conta

--- reactjs-system-chamado/src/pages/SignUp/index.js
+++ reactjs-system-chamado/src/pages/SignUp/index.js
@@ -1,7 +1,49 @@
+import { useState } from 'react'
+import './signup.css'
+
+import logo from '../../assets/intergard.png'
+import { Link } from 'react-router-dom'
+
+
 export default function SignUp() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+
     return (
-        <div>
-            <h1>Página de Cadastro</h1>
+        <div className='container-center'>
+           <div className='login'>
+            <div className='login-area'>
+                <img src={logo} alt='Logo da Intergard do Brasil' />
+            </div>
+            <form>
+                <h1>Criar Cadastrar</h1>
+
+                <input 
+                    type="text" 
+                    placeholder='Digite seu nome' 
+                    value={name} onChange={(e) => setName(e.target.value)} 
+                    />
+
+                <input 
+                    type="text" 
+                    placeholder='Digite seu e-mail' 
+                    value={email} onChange={(e) => setEmail(e.target.value)} 
+                    />
+
+                <input 
+                    type="password" 
+                    placeholder='********'
+                    value={password} onChange={(e) => setPassword(e.target.value)} 
+                    />
+
+                <button type='submit'>Cadastrar</button>
+            </form>
+            <Link to="/">Já possuo uma conta!</Link>
+
+           </div>
         </div>
     )
 }
