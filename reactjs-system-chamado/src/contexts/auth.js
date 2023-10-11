af7e108a9e5145335808b40159ccf3dd4e2cdeda
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-10-11 15:16:05 -0300

    Private

--- reactjs-system-chamado/src/contexts/auth.js
+++ reactjs-system-chamado/src/contexts/auth.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect } from "react";
 import { auth, db } from "../services/firebaseConnection";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
 import { useNavigate } from "react-router";
@@ -11,8 +11,24 @@ export const AuthContext = createContext({});
 function AuthProvider({ children }){
     const [user, setUser] = useState(null);
     const [loadingAuth, setLoadingAuth] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     const navigate = useNavigate();
+
+    useEffect( () => {
+        async function loadUser(){
+            const storageUser = localStorage.getItem('@ticketsPRO');
+
+            if(storageUser){
+                setUser(JSON.parse(storageUser));
+                setLoading(false);
+            }
+
+            setLoading(false);
+        }    
+        loadUser();
+    }, []);
+
 
 async function signIn(email, password){
         setLoadingAuth(true);
@@ -83,8 +99,25 @@ async function signIn(email, password){
         localStorage.setItem('@ticketsPRO', JSON.stringify(data));
     }
 
+    async function logout(){
+        await auth.signOut(auth);
+        localStorage.removeItem('@ticketsPRO');
+        setUser(null);
+        navigate('/');
+
+    }
+
     return(
-        <AuthContext.Provider value={{signed: !!user, user, signIn, signUp, loadingAuth}}>
+        <AuthContext.Provider 
+        value={{signed: !!user,
+        user,
+        signIn, 
+        signUp,
+        logout,
+        loadingAuth,
+        loading
+        }}
+        >
             {children}
         </AuthContext.Provider>
     )
