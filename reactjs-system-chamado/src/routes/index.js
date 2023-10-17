3cd6cb47ec6078f2cb3e637fa218e8ed4f56589f
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-10-17 14:48:57 -0300

    componente title

--- reactjs-system-chamado/src/routes/index.js
+++ reactjs-system-chamado/src/routes/index.js
@@ -5,6 +5,7 @@ import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 
 import Private from './Private';
+import Profile from '../pages/Profile';
 
 function RoutesApp(){
     return(
@@ -12,6 +13,7 @@ function RoutesApp(){
             <Route path="/" element={<SignIn/>}/>
             <Route path="/register" element={<SignUp/>}/>
             <Route path="/dashboard" element={ <Private><Dashboard/></Private>}/>
+            <Route path="/profile" element={ <Private><Profile/></Private>}/>
         </Routes>
     )
 }
