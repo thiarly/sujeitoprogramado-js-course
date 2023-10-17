b9010bc11ed5eae210d1a0f6abbc423f04d6cab9
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-10-17 14:16:40 -0300

    sidebar

--- reactjs-system-chamado/src/pages/Dashboard/index.js
+++ reactjs-system-chamado/src/pages/Dashboard/index.js
@@ -2,6 +2,8 @@ import { useContext } from "react"
 import { AuthContext } from "../../contexts/auth"
 
 
+import Header from "../../components/Header";
+
 export default function Dashboard() {
     const { logout } = useContext(AuthContext);
 
@@ -11,6 +13,7 @@ export default function Dashboard() {
 
     return (
         <div>
+            <Header />
             <h1>Dashboard</h1>
             <button onClick={ handleLogout}>Sair da Conta</button>
         </div>
