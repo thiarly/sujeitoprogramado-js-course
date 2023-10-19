10c0bf49d22f057bbb4d869a4c9999422d563800
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-10-19 11:55:42 -0300

    Buscando chamados

--- reactjs-system-chamado/src/pages/Dashboard/index.js
+++ reactjs-system-chamado/src/pages/Dashboard/index.js
@@ -6,20 +6,81 @@ import Title from "../../components/Title";
 import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi";
 
 import { Link } from "react-router-dom";
+import { collection, getDocs, orderBy, limit, startAfter, query } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+
+import { format } from "date-fns";
+
 
 import './dashboard.css'
 
-
+const listRef = collection(db, "chamados");
 
 export default function Dashboard() {
     const { logout } = useContext(AuthContext);
 
-    const [chamado , setChamado] = useState([{}]);
+    const [chamados , setChamados] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [isEmpty, setIsEmpty] = useState(false);
 
-   async function handleLogout() {
-       await logout();
+    useEffect(() => {
+        async function loadChamados(){
+            const q = query(listRef, orderBy('created', 'desc'), limit(10));
+
+            const querySnapshot = await getDocs(q);
+            await updateState(querySnapshot);
+
+            setLoading(false);
+        }
+        loadChamados();
+
+        return () => {};
+
+    }, []);
+
+    async function updateState(querySnapshot){
+        const isCollectionEmpty = querySnapshot.size === 0;
+
+        if (!isCollectionEmpty) {
+            let lista = [];
+
+            querySnapshot.forEach((doc) => {
+                lista.push({
+                    id: doc.id,
+                    assunto: doc.data().assunto,
+                    cliente: doc.data().cliente,
+                    clienteId: doc.data().clienteId,
+                    created: doc.data().created,
+                    createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+                    status: doc.data().status,
+                    complemento: doc.data().complemento
+                });
+            });
+
+            setChamados(chamados => [...chamados, ...lista]);
+        }else{
+            setIsEmpty(true);
+        }
     }
+
+    if (loading) {
+        return (
+            <div>
+                <Header />
+
+                <div className="content">
+                    <Title name="Tickets">
+                        <FiMessageSquare size={25} />
+                    </Title>
+                    
+                    <div className="container dashboard">
+                        <span>Buscando chamados...</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
 
     return (
         <div>
@@ -32,7 +93,7 @@ export default function Dashboard() {
                 
                 <>           
             
-                    {chamado.length === 0 ? (
+                    {chamados.length === 0 ? (
                         <div className="container dashboard">
                             <span>Nenhum chamado encontrado...</span>
                             <Link to="/new" className="new">  
@@ -58,26 +119,30 @@ export default function Dashboard() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td data-label="Cliente">Thiarly TC</td>
-                                <td data-label="Assunto">Vamos programar 1</td>
-                                <td data-label="Status">
-                                    <span className="badge" style={{ backgroundColor: '#999'}}>
-                                        Em progresso
-                                    </span>
-                                </td>
-                                <td data-label="Cadastrador">30/10/2023</td>
-                                <td data-label="#">
-                                    <button className="action" style={{ backgroundColor: "#3583f6"}}>
-                                        <FiSearch color='#FFF' size={17} />
-                                    </button>
-
-                                    <button className="action" style={{ backgroundColor: "#f6a935"}}>
-                                        <FiEdit2 color='#FFF' size={17} />
-                                    </button>
-                                </td>
-                            </tr>
-
+                            {chamados.map((item, index) => {
+                            
+                                return (
+                                <tr key={index}>
+                                        <td data-label="Cliente">{item.cliente}</td>
+                                        <td data-label="Assunto">{item.assunto}</td>
+                                        <td data-label="Status">
+                                            <span className="badge" style={{ backgroundColor: '#999'}}>
+                                                {item.status}
+                                            </span>
+                                        </td>
+                                        <td data-label="Cadastrador">{item.createdFormated}</td>
+                                        <td data-label="#">
+                                            <button className="action" style={{ backgroundColor: "#3583f6"}}>
+                                                <FiSearch color='#FFF' size={17} />
+                                            </button>
+        
+                                            <button className="action" style={{ backgroundColor: "#f6a935"}}>
+                                                <FiEdit2 color='#FFF' size={17} />
+                                            </button>
+                                        </td>
+                                    </tr>
+                            )
+                            })}
 
                         </tbody>
                     </table>
